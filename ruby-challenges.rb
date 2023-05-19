2ffdd40fb7dc87629ffb867bfd83bb183c568bda
wabowen89 <wabowen89@gmail.com>
2023-05-19 14:01:55 -0700

    Updated ruby-challeges

--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -23,16 +23,64 @@ filter_letter_o = 'o'
 filter_letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
-def word_contains (array, string)
-    tempArr = []
+# def word_contains (array, string)
+#     tempArr = []
     
-    array.each do |value|
-        if value.include? string
-            tempArr << value
-        end
-    end
+#     # .each iterates through the array
+#     array.each do |value|
 
-    p tempArr
+#         # Checks to see if 'string' is in the current value
+#         if value.include? string
+
+#             # Pushes current value to 'tempArr' if true
+#             tempArr << value
+#         end
+#     end
+
+#     p tempArr
+
+# end
+
+# word_contains(beverages_array, filter_letter_o)
+# word_contains(beverages_array, filter_letter_t)
+
+# Refactor 1
+
+# Pseudo Code:
+
+    # Process:
+        # Remove .each block
+        # Use .filter to iterate through given array
+        # Assign .filter results directly to tempArr
+    # Expected Output: ['coffee', 'soda water']
+                     # ['tea', 'water', 'soda water']
+
+# def word_contains (array, string)
+    
+#     #  .filter iterates through 'array' and checks to see if the current value contains the given string
+#     tempArr = array.filter {|value| value.include? string}
+
+#     p tempArr
+
+# end
+
+# word_contains(beverages_array, filter_letter_o)
+# word_contains(beverages_array, filter_letter_t)
+
+# Refactor 2
+
+# Pseudo Code:
+
+    # Process:
+        # Remove 'tempArr'
+        # Print array.filter directly
+    # Expected Output: ['coffee', 'soda water']
+                     # ['tea', 'water', 'soda water']
+
+def word_contains (array, string)
+    
+    #  Prints results of .filter directly
+    p array.filter {|value| value.include? string}
 
 end
 
@@ -51,21 +99,46 @@ word_contains(beverages_array, filter_letter_t)
         # Assign parameter names 'hash'
         # Create temporary 'array' and assign it the hash values (possible tool: .values)
         # Remove all nested arrays from 'array' variable (possible tool: .flatten)
+        # Sort 'array' alphabetically (possible tool: .sort)
         # Return the resulting array
     # Expected Output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
 us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
+# def createArray hash
+
+#     # Converts 'hash' into an array of the values with .values
+#     array = hash.values
+
+#     # Removes nested arrays with .flatten
+#     array = array.flatten
+
+#     # Sorts array alphabetically
+#     array.sort!
+
+#     p array
+# end
+
+# createArray(us_states)
+
+# Refactor
+# Pseudo Code:
+    # Process:
+        # Remove 'array' variable
+        # Combine .values, .flatten, and .sort methods
+    # Expected Output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
+
 def createArray hash
-    array = hash.values
-    array = array.flatten
-    p array
+
+    # .values converts 'hash' into an array of the hash's values
+    # .flatten removes nested arrays within the array
+    # .sort! sorts the array alphabetically
+    p hash.values.flatten.sort!
+
 end
 
 createArray(us_states)
-
-# Refactor
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
@@ -104,6 +177,11 @@ createArray(us_states)
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
+# Expected output example: my_bike.pedal_faster(10) => 10
+# Expected output example: my_bike.pedal_faster(18) => 28
+# Expected output example: my_bike.brake(5) => 23
+# Expected output example: my_bike.brake(25) => 0
+
 # Pseudo Code: 
     # Method Name: pedal_faster
         # Input: Integer
@@ -124,30 +202,93 @@ createArray(us_states)
         # Expected Output: 23
                          # 0
 
+# class Bike
+
+#     # Creates getter/setter methods for variables
+#     attr_accessor :model, :wheels, :current_speed
+
+#     # Establishes base variables for the object to be created
+#     def initialize (model)
+#         @model = model
+#         @wheels = 2
+#         @current_speed = 0
+#     end
+
+#     # Prints the current info contained for the bike object
+#     def bike_info
+#         p "The #{@model} has #{@wheels} wheels and is going #{current_speed} mph."
+#     end
+
+#     # Increases 'current_speed' variable
+#     def pedal_faster (integer)
+#         @current_speed = @current_speed + integer
+#     end
+
+#     # Decreases 'current_speed' variable
+#     # Prevents 'current_speed' value from dropping below 0
+#     def brake (integer)
+
+#         # Checks to see if 'current_speed' minus 'integer' is below 0
+#         if (@current_speed - integer < 0)
+#             @current_speed = 0
+#         else
+#             @current_speed = @current_speed - integer
+#         end
+#     end
+# end
+
+# trek = Bike.new('Trek')
+# trek.bike_info
+
+# trek.pedal_faster(10)
+# trek.bike_info
+
+# trek.pedal_faster(18)
+# trek.bike_info
+
+# trek.brake(5)
+# trek.bike_info
+
+# trek.brake(25)
+# trek.bike_info
+
+# Refactor
+# Pseudo Code:
+    # Process:
+        # Use ternary operate in place of if/else statments
+        # Use += and -= for increasing and decreasing 'current_speed'
+    # Expected Output: 23
+                     # 0
+
 class Bike
 
+    # Creates getter/setter methods for variables
     attr_accessor :model, :wheels, :current_speed
 
+    # Establishes base variables for the object to be created
     def initialize (model)
         @model = model
         @wheels = 2
         @current_speed = 0
     end
 
+    # Prints the current info contained for the bike object
     def bike_info
         p "The #{@model} has #{@wheels} wheels and is going #{current_speed} mph."
     end
 
+    # Increases 'current_speed' variable
     def pedal_faster (integer)
-        @current_speed = @current_speed + integer
+        @current_speed += integer
     end
 
+    # Decreases 'current_speed' variable
+    # Prevents 'current_speed' value from dropping below 0
     def brake (integer)
-        if (@current_speed - integer < 0)
-            @current_speed = 0
-        else
-            @current_speed = @current_speed - integer
-        end
+
+        # Ternary operator puts if/else statement on one line
+        (@current_speed - integer) < 0 ? @current_speed = 0 : @current_speed -= integer    
+
     end
 end
 
@@ -165,9 +306,3 @@ trek.bike_info
 
 trek.brake(25)
 trek.bike_info
-
-
-# Expected output example: my_bike.pedal_faster(10) => 10
-# Expected output example: my_bike.pedal_faster(18) => 28
-# Expected output example: my_bike.brake(5) => 23
-# Expected output example: my_bike.brake(25) => 0
